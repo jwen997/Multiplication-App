0e17e46d4e825c39aef4eb788382f5050dc470c9
jwen997 <117602830+jwen997@users.noreply.github.com>
2023-01-07 15:06:25 -0500

    app

---
+++ index.js
@@ -0,0 +1,14 @@
+const num1 = Math.ceil(Math.random()*10)
+const num2 = Math.ceil(Math.random()*10)
+
+const questioE1 = document.getElementById("question");
+
+const formE1 = document.getElementById("form")
+
+questioE1.innerText = 'What is ${num1} multiply by ${num2}
+
+const correctAns = num1 * num2;
+
+formE1.addEventListener("submit", ()=>{
+
+})
